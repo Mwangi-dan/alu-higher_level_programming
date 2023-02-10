450948535a6e1d24849293e609196a220cdfb9d8
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-10 05:15:24 -0800

    mile in the mirroe

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-
-for i in range(122, 97):
+i = 0
+for j in range(ord('z'), ord('a')- 1, -1):
+    print("{}".format(chr(j - i)), end="")
+    if i == 0:
+        i = 32
+    else:
+        i = 0
     
