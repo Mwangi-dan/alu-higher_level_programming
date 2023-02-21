65a8d43b20d58e2d6a6e5219690b7be8b4d0bb68
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-21 14:36:37 -0800

    Infinite addition

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # script to add integer arguments
 import sys
-a = sys.argv
-add = 0
-for i in range(1, len(a)):
-    num = int(a[i])
-    add += num
-print(add)
+if __name__ == "__main__":
+    a = sys.argv
+    add = 0
+    for i in range(1, len(a)):
+        num = int(a[i])
+        add += num
+    print(add)
