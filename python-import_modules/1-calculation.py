9213427bdacf592621594351f8db690a27e696e0
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-21 12:03:19 -0800

    My first toolbox

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     b = 5
     # printing the operations
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
