f53ac2eefdd962176938719d263fb3f8b240e3bd
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-21 15:36:35 -0800

    Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
-#!/usr.bin.python3
-# Scrit that prints all names defines by the compiled module
+#!/usr/bin/python3
+# Script that prints all names defines by the compiled module
 
 import hidden_4
 if __name__ == "__main__":
-    
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print("{}".format(i))
