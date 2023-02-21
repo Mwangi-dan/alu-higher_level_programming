a9739e243b21d3e4db1ca5d97de91ac8a612375f
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-21 11:44:56 -0800

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # script that impirts function from another file
-import add_0 as add
-sum = add.add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, sum(a, b)))
+if __name__ == "__main__":
+    import add_0 as add
+    sum = add.add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, sum(a, b)))
