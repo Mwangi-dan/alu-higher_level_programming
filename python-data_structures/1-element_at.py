007334eac848ae644fecf976aa74de9aeb9311a9
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-26 16:07:37 -0800

    Secure access to an element in a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 # script to access element at a particlar point in list
 
 
-def element_at(my_list.idx):
+def element_at(my_list, idx):
     if (idx < 0):
         return None
     elif (idx > len(my_list)):
