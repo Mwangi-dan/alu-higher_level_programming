53c7471c81e0c4e3456a3e297e9371d9e7ae53bb
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-28 03:41:01 -0800

    Replace a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -8,6 +8,6 @@ def new_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return my_list
     else:
-        if i == idx:
-            my_list[i] = element
-
+        for i in range(len(my_list)):
+            if i == idx:
+                my_list[i] = element
