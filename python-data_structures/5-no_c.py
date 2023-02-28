44cfbffd307c959c3bc42121f5a81fc06590d62c
Mwangi-dan <d.ndungu@alustudent.com>
2023-02-28 04:15:48 -0800

    Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@
 def no_c(my_string):
     new_str = ''
     for i in range(len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
+        if my_string[i] != 'c' or my_string[i] != 'C':
             new_str += ("{:s}".format(my_string[i]))
     return new_str
