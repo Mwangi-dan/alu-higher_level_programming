18a92f9cd641faa719c8dd8daea27e23916ea8e0
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-06 11:26:39 -0800

    Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,9 +4,11 @@
 
 def divisible_by_2(my_list=[]):
     if my_list:
+        new_list = []
         for i in my_list:
             if i % 2 == 0:
-                i = True
+                new_list += True
             else:
-                i = False
+                new_list += False
+        return new_list
     return None
