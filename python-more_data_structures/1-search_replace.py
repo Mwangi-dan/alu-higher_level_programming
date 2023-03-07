f6ad160969e19034a101abfe839407b06df3b66a
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-07 05:38:20 -0800

    Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # function that replaces all occurrences of an element in new list
 
+
 def search_replace(my_list, search, replace):
-    my_list = list(map(lambda x: replace if x == search, my_list else x, my_list))
+    my_list = list(map(lambda x: replace if x == search else x, my_list))
     return my_list
