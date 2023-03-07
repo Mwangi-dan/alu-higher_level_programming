8a6a7408edbe93a82dd67d7aa664f90560deddd7
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-07 05:07:50 -0800

    List of lists: matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" " if matrix[i][j] != matrix[i][-1] else "")
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
