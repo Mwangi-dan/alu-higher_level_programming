b3350e48abac1201cc2bbeca641c5c3508735d91
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-07 05:29:09 -0800

    Search and replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# function that replaces all occurrences of an element in new list
+
+def search_replace(my_list, search, replace):
+    my_list = list(map(lambda x: replace if x == search, my_list else my_list))
+    return my_list
