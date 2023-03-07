d09ae50cfba440535a5542cadbfe5487e54c1aff
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-07 02:19:05 -0800

    Squared simple

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# function that completes square if all integers of matrix
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([map(lambda a: a ** 2, row)])
+    return new_matrix
