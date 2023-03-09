be79096bd1f24dd669d859b8bb45ac9481fb34de
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-09 09:09:22 -0800

    Roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     pre = 0
     res = 0
-    if roman_string != None and roman_string:
+    if type(roman_string) is str and roman_string:
         for i in range(len(roman_string) - 1, -1, -1):
             if val[roman_string[i]] >= pre:
                 res += val[roman_string[i]]
