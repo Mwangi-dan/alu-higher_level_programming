575bbd35749805f55df7f944b51fba899045e5c5
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-09 15:15:27 -0800

    Safe list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    length = 0
+    count = 0
+    for i in my_list:
+        length += 1
+        for j in range(x):
+            try:
+                print("{}".format(my_list[j]), end="")
+            except:
+                print("")
+                return length
+            count +=
+        print("")
+        return count
