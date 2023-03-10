4e41dc0d7eeb343d3d30e2bcb833a7aa6275d3cf
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-10 02:59:37 -0800

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,4 +2,11 @@
 
 
 def safe_print_division(a, b):
+    try:
+        res = a / b
+    except(TypeError, ZeroDivisionError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
 
