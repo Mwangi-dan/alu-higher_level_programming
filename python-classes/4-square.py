4cc7d2f64a404a571b196a42bfc5fe87b63dafb8
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-15 07:34:58 -0700

    Accesand update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Module for class Square
+
+"""
+
+
+class Square:
+    """Square Class
+
+    This is a square class
+
+    """
+
+    def __init__(self, size=0):
+        """__init__
+
+        The __init__ methid initialies the size value of the square.
+
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+
+        Raises:
+            TypeError: If `size` type is not `int`
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area
+
+        """
+        return self.__size ** 2
+
+    @property
+    def __get_size(self):
+        """
+        Retrieves the size
+        
+        """
+        return self.__size
+
+    @size.setter
+    def __set_x(self, size):
+        """
+        Sets the value of size
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
+
