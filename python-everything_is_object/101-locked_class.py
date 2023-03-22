5b46b64eee9c9e6d9c8754aec361dc66da4c3f1d
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-22 05:37:02 -0700

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Locked Class
+This class has no class or object attribute that prevents
+user from dynamically creating new instance attributes
 
+Args:
+    first_name: (str) Takes in the name
+
+"""
 
 class LockedClass:
     if __name__ == "__first_name__":
