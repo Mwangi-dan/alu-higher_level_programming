ca5fc2c36c960ae0fb1d97965dca69b31fc4405e
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-22 05:45:50 -0700

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Locked Class
-This class has no class or object attribute that prevents
-user from dynamically creating new instance attributes
-
-Args:
-    first_name: (str) Takes in the name
-
 """
+Defines a Locked Class
+"""
+
 
 class LockedClass:
     """
-    Class for the LockedClass
+    Prevents the user from instantiating new LockedClass attributes 
+    for anything but attributes called 'first_name'
     """
-    if __name__ == "__first_name__":
-       def first_name(self, name):
-           self.name = name
+    __slots__ = ["first_name"]
