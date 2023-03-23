2a0112b69ae36766ee61a3921db4b137d027e10f
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-23 07:39:49 -0700

    Only subclass of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""
+Module to check if object is an instance of class that inherited
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function to check if `obj` is a subclass of `a_class`
+
+    Args:
+        obj (any): The object to compare
+        a_class (class): Class to comapre with object
+
+    Returns:
+       `True`: if `obj` is a subclass of `a_class`
+       `False`: if `obj` is not a subclass of `a_class`
+
+    """
+    return issubclass(obj, a_class)
