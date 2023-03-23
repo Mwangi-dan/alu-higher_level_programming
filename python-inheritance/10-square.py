1594113921ca9fd4809efccf1113b21eb7e279a7
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-23 12:59:00 -0700

    Square #1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 Square class that inherits from rectangle
+and BaseGeometry
+
 """
 Rectangle = __import__('9-rectangle.py').Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
