d861e148bb511f7e4742d07cbe840765ee2b9ac1
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-23 16:09:28 -0700

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,14 +5,14 @@ Module to add arguments to python list and save them to a file
 """
 from os import path
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if path.exists('add_item.json'):
     obj_json_file = load_from_json_file('add-item.json')
 else:
     obj_json_file =[]
 
-for i in range(1, len(argv):
+for i in range(1, len(argv)):
     obj_json_file.append(argv[i])
 save_to_json_file(obj_json_file, 'add_item.json')
