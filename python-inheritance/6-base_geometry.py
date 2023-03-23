0dc9d4aaee94b82aade2d7ffa0f6052fc8b6c6f4
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-23 07:57:25 -0700

    Imprive Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Base Geometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Raises exception
+        
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
