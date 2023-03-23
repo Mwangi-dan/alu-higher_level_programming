6b1fc9f70b77f267923d57e5c5561b9891a53aa0
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-23 14:16:42 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,13 @@ Function reads text and prints to stdout
 
 
 def read_file(filename=""):
+    """
+    function to open filename and print its contents
+    
+    Args:
+        filename (string): name of file
+    
+    """
     with open('filename' 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end='')
