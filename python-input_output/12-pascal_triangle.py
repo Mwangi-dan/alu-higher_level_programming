8bfef6685a9fecba8c01114a061505f85083f81b
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-24 14:09:04 -0700

    Pascals triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,9 +10,9 @@ def pascal_triangle(n):
 
     Returns a list of lists of integeres representing the triangle
     """
-    if n <= 0:
-        return []
     li = []
+    if n <= 0:
+        return li
     for r in range(n):
         for c in range(r + 1):
             if c == 0:
@@ -20,5 +20,5 @@ def pascal_triangle(n):
             elif c == r:
                 li[r].append(1)
             else:
-                li[r].append(li[r -1][c] + li[r - 1][c - 1])
+                li[r].append(li[r - 1][c] + li[r - 1][c - 1])
         return li
