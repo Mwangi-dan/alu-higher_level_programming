bb5426a0fd21c9894a33cc98db29a536d4a6ab1e
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-24 12:02:57 -0700

    Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module with class student
+"""
+
+
+class Student:
+    """
+    Class defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization methid
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Method that returns directory description
+        """
+        res = {}
+        if hasattr(self, "__dict__"):
+            res = self.__dict__.copy()
+        return res 
