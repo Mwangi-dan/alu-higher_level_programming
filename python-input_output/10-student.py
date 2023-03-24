4edd781adcbdeec0123142b2d8a54317bc58ec0e
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-24 12:13:12 -0700

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module with class student
+"""
+
+
+class Student:
+    """
+    Class defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization methid
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method that returns directory description
+        """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            res = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] = satr:
+                        res[satr] = obj[satr]
+            return res
