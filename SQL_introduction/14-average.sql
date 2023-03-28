5c18dd71bb9173611b0899d7d973fa2295aeda20
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-28 05:07:39 -0700

    Score too low

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- omputes the score average of all records in the table `second_table`
-ALTER TABLE second_table ADD average (SELECT AVG(score) FROM second_table);
+SELECT AVG(score) FROM second_table;
