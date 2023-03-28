02e75ac6d4ceb6df98ceb969d2bd534da02f6807
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-28 04:39:44 -0700

    List by best

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- List all records of table `second_table`
-SELECT score, name FROM second_table GROUP BY score;
+-- List all records of table `second table`
+SELECT score, name FROM second_table ORDER BY score DESC;
