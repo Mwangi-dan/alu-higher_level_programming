e722b3583b1e555c566a47ec96812c20637644ba
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-28 03:28:19 -0700

    First table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- Creates a tabk=le in current database
-USE hbtn_test_db_4;
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256),
+CREATE TABLE IF NOT EXISTS `first_table` (
+	`id` INT,
+	`name` VARCHAR(256),
 );
