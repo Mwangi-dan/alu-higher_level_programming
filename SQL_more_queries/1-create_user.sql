cbddae1bda3037ab0c43882f5366e64ad31fb72a
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-30 06:05:11 -0700

    Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates MYSQL server user_0d_1
 -- `user_0d_1` has all privileges 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
