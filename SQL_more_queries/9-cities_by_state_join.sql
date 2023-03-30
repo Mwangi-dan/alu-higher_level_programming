33c94484a59db19398e678aa6a4d6040620bd6c0
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-30 11:45:17 -0700

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Lists that lists all cities in database `hbtn_0d_usa`
-SELECT cities.id cities.name states.name FROM states, cities ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id;
