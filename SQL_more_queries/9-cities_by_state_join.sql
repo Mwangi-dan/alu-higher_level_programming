65454626bf9c263cf774220fbe982799cef71a51
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-30 11:50:32 -0700

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Lists that lists all cities in database `hbtn_0d_usa`
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = state.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id;
