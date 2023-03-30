cfd34a699b5208bb6079d40d5e64bd955371a973
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-30 11:01:00 -0700

    Vities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- List all cities of california in database `hbtn_0d_usa`
-SELECT * FROM states WHERE name = 'California' GROUP BY cities.id;
+SELECT * FROM states WHERE name = 'California' ORDER BY cities.id ASC;
