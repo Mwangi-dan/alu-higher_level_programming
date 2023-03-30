e69f418d1c10eeb2a64b6bb16b686f081afb5ddf
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-30 06:12:08 -0700

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Creates database `hbtn_0d_2`
 -- Creates user 'user_0d_2` with SELECT privilege
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-USE DATABASE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
