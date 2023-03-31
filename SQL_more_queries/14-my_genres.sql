ee47f4bdbef39ecb33e3ca16ca74cc8dabf30115
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 07:00:20 -0700

    My genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- Lists all genres of show `Dexter`
 SELECT name FROM tv_genres
-JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_genres.id
+JOIN tv_shows ON tv_shows_id = tv_show_genres.show_id
 ORDER BY tv_genres.name;
