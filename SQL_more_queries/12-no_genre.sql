f58eff8bc3565a3f7a64c02989f4492eff71914d
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 05:55:13 -0700

    No genre

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 -- Shows with genre_id as NULL
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_show.id = tv_show_genres.show_id
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title
