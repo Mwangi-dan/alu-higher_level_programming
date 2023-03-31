a718c9ca61656d69edde9ef11c7b7e2d677cfaa8
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 11:10:50 -0700

    Not my genre

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,8 @@
 -- Lists all genres no listed to show `Dexter`
 SELECT name FROM tv_genres
-LEFT JOIN tv_show_genres ON id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
+WHERE tv_genres.id NOT IN (
+	SELECT tv_genres.id FROM tv_genres
+	JOIN tv_show_genres ON id = tv_show_genres.genre_id
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = 'Dexter' )
 ORDER BY name;
