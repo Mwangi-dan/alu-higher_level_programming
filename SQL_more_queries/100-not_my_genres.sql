735bd844eba99bc1c04a03ee7f0baf719a90a985
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 11:03:55 -0700

    Not my genre

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -2,4 +2,5 @@
 SELECT name FROM tv_genres
 RIGHT JOIN tv_show_genres ON id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY name;
