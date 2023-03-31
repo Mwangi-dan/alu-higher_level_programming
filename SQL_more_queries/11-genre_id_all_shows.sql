25f18f239dc0aac448b7894765fdaf568089c512
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 05:41:35 -0700

    Genre ID by show

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in the database `hbtn_0d_tvshows`
+-- If show doesnt have a genre, it displays NULL
+SELECT tv_shows.title, tv_show_genre.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id
