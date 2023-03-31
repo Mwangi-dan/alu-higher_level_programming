190ee6726ce44ea33760c1fdac3b9d3ff1f16a6c
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 11:20:21 -0700

    No comedy tonight

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- Lists all shows without genre `Comedy`
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id NOT (
+	SELECT tv_show_genres.genre_id FROM tv_show_genres
+	JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres <> 'Comedy' )
+ORDER BY title;
