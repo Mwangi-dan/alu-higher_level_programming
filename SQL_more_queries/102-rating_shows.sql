9351d8682ba6922e5e009e62b7e3297068712d8d
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 11:53:08 -0700

    Rotten tomatoes

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 --  lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT title, COUNT(*) AS 'rating sum' FROM tv_shows
+SELECT title, SUM(rate) AS 'rating' FROM tv_shows
 JOIN tv_show_ratings ON id = tv_show_ratings.show_id
-GROUP BY tv_show_ratings.rate
-ORDER BY tv_show_ratings.rate;
+GROUP BY tv_shows.title
+ORDER BY tv_show_ratings.rate DESC;
