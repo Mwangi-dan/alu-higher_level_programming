7cabdd6d8b8b7f511cf067a8f32c128b7484c7b5
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 05:29:39 -0700

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- Lists all shows contained in `hbtn_0d_tvshows`
 -- Each record should display `tv_shows.title and `tv_show_genres.genre.id`
 SELECT * FROM tv_shows
-FULL JOIN tv_shows
-ON `tv_shows.id` = `tv_show_genres.show_id`
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
 
