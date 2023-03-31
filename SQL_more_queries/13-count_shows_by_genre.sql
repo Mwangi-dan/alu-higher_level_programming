8e9298b6504b2fb82927067c557621fc677d67e7
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 06:30:43 -0700

    Number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all genres and displays number of shows linked to each
-SELECT tv_genre.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 JOIN tv_show_genres ON id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
