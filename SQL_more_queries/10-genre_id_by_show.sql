b02236784c24a49229b8f9226a7d01254662e682
Mwangi-dan <d.ndungu@alustudent.com>
2023-03-31 05:25:28 -0700

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,5 @@
 -- Each record should display `tv_shows.title and `tv_show_genres.genre.id`
 SELECT * FROM tv_shows
 FULL JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
+ON `tv_shows.id` = `tv_show_genres.show_id`
+
