0873c2f7c5cecbb2ee2165cb2c82f6b8f6d82c1c
Mwangi-dan <d.ndungu@alustudent.com>
2023-04-07 11:54:48 +0000

    cURL POST parameters

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #cURL post parameters
-curl -sX POST $1 -d "email:test@gmail.com&subject:I will always be here for PLD"
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
