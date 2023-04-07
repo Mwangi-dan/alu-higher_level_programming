0afb6f6d9ddec335389292ec62affe3505043cf6
Mwangi-dan <d.ndungu@alustudent.com>
2023-04-07 14:45:30 +0000

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,5 +10,5 @@ with urllib.request.urlopen(url) as response:
 	body = respnse.read()
 	print("Body response:")
 	print("\t- type : {}".format(type(body)))
-	print("\t- content: {}".format(body)))
+	print("\t- content: {}".format(body))
 	print("\t- utf8 content: {}".format(body.decode('utf-8')))
