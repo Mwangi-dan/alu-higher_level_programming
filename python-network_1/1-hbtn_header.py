db4c128cf31d5fc0063e4b55801dc5ce6515adb5
Mwangi-dan <d.ndungu@alustudent.com>
2023-04-07 19:37:06 +0000

    Error code #0

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,10 +5,13 @@ value of the X-Request-Id variable found in the header of the
 response
 """
 import urllib.request
-import sys
+from sys import argv
 
-url = sys.argv[1]
 
-request = urllib.request.Request(url)
-with urllib.request.urlopen(request) as resp:
-	print(dict(resp.headers).get("X-Request-Id"))
+if __name__ == "__main__":
+    """takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header
+    of the response"""
+    with urllib.request.urlopen(argv[1]) as response:
+        html_id = response.info().get('X-Request-Id')
+        print(html_id)
