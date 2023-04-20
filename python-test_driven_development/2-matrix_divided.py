7c342e66090bdec79f5ece9a5ab72dcac91ff999
Mwangi-dan <d.ndungu@alustudent.com>
2023-04-20 13:19:44 +0200

    Divide a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -19,19 +19,20 @@ def matrix_divided(matrix, div):
 
     :return: new matrix (list of lists)
     """
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(error_msg)
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(error_msg)
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(error_msg)
             
     return [[round(element / div, 2) for element in row] for row in matrix]
