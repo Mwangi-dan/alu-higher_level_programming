31c90a4101a4cafcdbc89e130d1d76e38474c17d
Mwangi-dan <d.ndungu@alustudent.com>
2023-04-20 14:38:11 +0200

    Print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,6 +3,7 @@
 Module with print_square function
 """
 
+
 def print_square(size):
     """
     Function to print a square with the character #
@@ -12,7 +13,7 @@ def print_square(size):
     """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size <= 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
     if type(size) is float and size < 0:
         raise TypeError("size must be an integer")
