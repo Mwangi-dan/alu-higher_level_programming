5a249eb7d69325f67c99de90a4b33882f76c8f13
Mwangi-dan <d.ndungu@alustudent.com>
2023-04-27 11:02:10 +0200

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states starting with passed arg, prevents injection"""
+"""Lists all cities by state"""
 
 import MySQLdb
 from sys import argv
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-        (argv[4], ))
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name FROM cities
+        LEFT JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC;
+        """)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
