1f25484b7c6a76de8f55328c82e5db68d56b9be6
root <d.ndungu@alustudent.com>
2023-06-14 03:21:08 -0700

    Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -6,7 +6,7 @@ function factorial (n) {
     return n * factorial(n - 1);
   }
 }
-if (Number.isNaN(process.argv[2])) {
+if (Number.isNaN(process.argv[2]) || process.argv.length === 2) {
   console.log('1');
 } else {
   console.log(factorial(process.argv[2]));
