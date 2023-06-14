73b32711176754ea9750b4178aee7db3620ece0d
root <d.ndungu@alustudent.com>
2023-06-14 03:46:56 -0700

    Second Biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 const argList = process.argv.slice(2);
-console.log(Math.max(argList.map(element => parseInt(element, 10))));
+const intList = argList.map(element => +element);
+console.log(Math.max(...intList));
