bded99c4a5c428014d95b6698ca60464e53ba157
root <d.ndungu@alustudent.com>
2023-06-14 01:05:03 -0700

    An integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstArgv = process.argv[2];
-if (firstArgv.isInteger()) {
-  console.log('My number: ' + firstArgv);
-} else {
+if (isNaN(firstArgv)) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + firstArgv);
 }
