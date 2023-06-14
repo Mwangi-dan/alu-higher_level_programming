65579907af65cc4142762e5153063892e3ca7ade
root <d.ndungu@alustudent.com>
2023-06-14 03:57:09 -0700

    Second Biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length > 2) {
   const argList = process.argv.slice(2);
   const intList = argList.map(element => +element).sort();
-  console.log(intList[-2]);
+  console.log(intList[intList.length - 2]);
 } else {
   console.log(0);
 }
