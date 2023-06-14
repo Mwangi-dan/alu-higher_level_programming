a12e7a864a68ee6c587adc32114f9900131d83c1
root <d.ndungu@alustudent.com>
2023-06-14 06:57:56 -0700

    Rectangle #3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -7,4 +7,15 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
+  print () {
+    let output = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        output += 'X';
+      }
+      console.log(output);
+      output = '';
+    }
+  }
 };
