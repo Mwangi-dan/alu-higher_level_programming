79b18d8fbd5ed278f85eeccf48cd911eed0f9301
root <d.ndungu@alustudent.com>
2023-06-14 01:30:26 -0700

    I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  let times = +process.argv[2];
+  const times = +process.argv[2];
   let i = 0;
   while (i < times) {
     console.log('C is fun');
