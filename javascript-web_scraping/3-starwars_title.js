9569eb37cfec9930df68dedd2eb5d6d82ccce8b2
root <d.ndungu@alustudent.com>
2023-06-15 14:18:27 +0000

    Star Wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const episodeNumber = process.argv[2];
+const requestURL = 'https://swapi-api.alx-tools.com/api/films/';
+request(requestURL, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const bodyJSON = JSON.parse(body);
+  const results = bodyJSON.results;
+  for (const result of results) {
+    if (result.episode_id === +episodeNumber) {
+      console.log(result.title);
+    }
+  }
+});
