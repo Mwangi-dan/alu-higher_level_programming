86a3edf88a6ddb7ad2b5ca604a72d1d86917f7cf
root <d.ndungu@alustudent.com>
2023-06-15 15:36:11 +0000

    Loripsum

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,14 +4,14 @@ const request = require('request');
 const fs = require('fs');
 const path = process.argv[3];
 request(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-    fs.writeFile(path, body, err => {
-        if (err) {
-            console.log(err)
-        }
-    })
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
