e795bb1e310e833496bf385fb3f916c028c62f5a
root <d.ndungu@alustudent.com>
2023-06-14 22:29:47 -0700

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Square class that inherits from square class in 5-square
+const Square5 = require('./5-square');
+
+module.extends = class Square extends Square5 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.size; i++) {
+        let output = '';
+        for (let j = 0; j < this.size; j++) {
+          output += 'C';
+        }
+        console.log(output);
+      }
+    } else {
+      super.print();
+    }
+  }
+};
