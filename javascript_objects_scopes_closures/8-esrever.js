1a3d1d70b938fb940ced022ab57215476cffb733
root <d.ndungu@alustudent.com>
2023-06-14 23:58:23 -0700

    Esrever

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Returns reversed version of list
+exports.esrever = function (list) {
+  const reverseList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverseList.push(list[i]);
+  }
+  return reverseList;
+};
